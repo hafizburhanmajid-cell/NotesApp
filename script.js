368dfbc2c53a401324094c254524287c1f695dd1
hafizburhanmajid-cell <hafizburhanmajid@gmail.com>
2026-08-19 02:03:33 -0700

    changes

--- script.js
+++ script.js
@@ -3,10 +3,12 @@ const notecontent=document.getElementById("note-content");
 const button=document.getElementById("btn");
 const notes=document.getElementById("notes");
 const form = document.getElementById("noteForm");
+const category=document.getElementById("filterCategory")
 form.addEventListener("submit",function(event){
     event.preventDefault();
     let titleValue=notetitle.value;
     let contentValue=notecontent.value;
+    let categoryValue=category.value;
     let newNote=document.createElement("div");
     newNote.className ="bg-black/40 border border-gray-300 rounded-xl p-5 mb-4 w-full shadow hover:shadow-lg transition-all";
     let newTitle = document.createElement("h3");
@@ -14,7 +16,11 @@ form.addEventListener("submit",function(event){
     newTitle.className ="text-2xl font-bold text-amber-950 mb-2";
     let newContent = document.createElement("p");
     newContent.textContent = contentValue;
-    newContent.className ="text-gray-900";
+    newContent.className ="text-gray-900 mb-7";
+    let icon =document.createElement("div");
+    let topRow=document.createElement("div");
+    topRow.className="flex justify-between  items-center mb-2"
+    icon.className="flex gap-2"
     let deletebtn=document.createElement("button");
     deletebtn.innerHTML='<i class="fa-solid fa-trash"></i>';
     deletebtn.className="hover:bg-white/40 text-black/50 hover:text-red-800 rounded cursor-pointer p-1";
@@ -27,7 +33,7 @@ form.addEventListener("submit",function(event){
     pin.addEventListener("click",function(){
         notes.prepend(newNote);
     });
-      let edit=document.createElement("button");
+    let edit=document.createElement("button");
     edit.innerHTML='<i class="fa-solid fa-pencil"></i>';
     edit.className="hover:bg-white/40 text-black/50 hover:text-black rounded cursor-pointer p-1";
     edit.addEventListener("click",function(){
@@ -35,15 +41,21 @@ form.addEventListener("submit",function(event){
         let newcontentvalue=prompt("Enter new content:");
         if(newtitleValue!==null){
             newTitle.textContent=newtitleValue;
-        } 
+        }
         if(newcontentvalue !== null){
             newContent.textContent=newcontentvalue;
         }
     });
-    newNote.append(newTitle);
+let newcategory=document.createElement("span");
+newcategory.textContent=categoryValue;
+newcategory.className="bg-white/30 hover:bg-white rounded-2xl cursor-pointer p-1 text-amber-950 px-2 text-sm";
+    topRow.append(newTitle);
+    topRow.append(icon);
+    icon.append(deletebtn);
+    icon.append(pin);
+    icon.append(edit);
+    newNote.append(topRow);
     newNote.append(newContent);
-    newNote.append(deletebtn);
-    newNote.append(pin);
-    newNote.append(edit);
+    newNote.append(newcategory);
     notes.append(newNote);
 });
